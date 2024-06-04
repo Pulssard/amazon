67490a00fef91d3cbd8c5218aefe14797e63bbea
Daniel <rdd254@gmail.com>
2024-06-04 22:49:25 +0300

    case insensitive for search

--- data/search.js
+++ data/search.js
@@ -1,6 +1,6 @@
 
 export function handleSearch(){//handles the search function
-    const searchParam = document.querySelector('.search-bar').value;//getting the value from the input
+    const searchParam = document.querySelector('.search-bar').value.toLowerCase();//getting the value from the input
 
     if(searchParam){
       const newUrl = `index.html?search=${searchParam}`;
