11a4fe86ad8f7cf4606ecb81f7f54e3cb8f67b70
Daniel <rdd254@gmail.com>
2024-06-11 15:24:02 +0300

    pop-up visual modification

--- scripts/utils/renderPopUp.js
+++ scripts/utils/renderPopUp.js
@@ -27,7 +27,7 @@ export function renderToastHTML(messageType = 'alert'){
         const html = `<div class="toast ${messageType}" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
           <i class="fas ${icon} fa-lg me-2"></i>
-          <strong class="me-auto">${messageType[0].toUpperCase() + messageType.slice(1,-1)}</strong>
+          <strong class="me-auto">${messageType[0].toUpperCase() + messageType.slice(1)}</strong>
           <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close">
             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23000'><path d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/></svg>
           </button>
